4331fa6804355187f1fbe303391336e4d8f481e8
Tilman <tilman@pop-os.localdomain>
2023-03-03 14:19:08 +0100

    Write script

--- src/whisper_to_transkripto.py
+++ src/whisper_to_transkripto.py
@@ -1,2 +1,35 @@
+import argparse
+import json
+import math
+
+
+def convert_timestamp(seconds):
+    return "#{hours:02d}:{minutes:02d}:{seconds:02d}-{decimal:01d}#".format(
+        hours=int(seconds // 3600),
+        minutes=int((seconds % 3600) // 60),
+        seconds=int(seconds % 60),
+        decimal=int(round(math.floor((seconds % 1) * 10), 0)),
+    )
+
+
+def convert_line(input, end):
+    return "{} {}".format(input.lstrip(), convert_timestamp(end))
+
+
 def convert_whisper_to_transcripto(input_file, output_file):
-    pass
+    input = json.load(input_file)
+    for segment in input["segments"]:
+        output_file.write(
+            "{}\n\n".format(convert_line(segment["text"], segment["end"]))
+        )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Convert the json file that is output from whisper to the format required by transkripto"
+    )
+    parser.add_argument("input_file", type=argparse.FileType("r"))
+    parser.add_argument("output_file", type=argparse.FileType("w+"))
+    args = parser.parse_args()
+
+    convert_whisper_to_transcripto(args.input_file, args.output_file)
--- test/test_whisper_to_transkripto.py
+++ test/test_whisper_to_transkripto.py
@@ -1,15 +1,43 @@
 import pytest
 import os
 import filecmp
-from src.whisper_to_transkripto import convert_whisper_to_transcripto
+from src.whisper_to_transkripto import (
+    convert_whisper_to_transcripto,
+    convert_line,
+    convert_timestamp,
+)
 
 
 # TODO: try to remove line breaks when speaker not changing? -> adapt in ground truth file
 def test_component():
     dir = os.path.dirname(__file__)
-    input_file = os.path.join(dir, "input.txt")
-    output_file = os.path.join(dir, "output.txt")
-    convert_whisper_to_transcripto(input_file, output_file)
-    assert filecmp.cmp(
-        os.path.join(dir, "expected_output.txt"), output_file, shallow=False
+    input_path = os.path.join(dir, "input.json")
+    output_path = os.path.join(dir, "output.txt")
+    with open(input_path, "r") as input_file, open(output_path, "w+") as output_file:
+        convert_whisper_to_transcripto(input_file, output_file)
+
+    expected_output_path = os.path.join(dir, "expected_output.txt")
+    same_content = filecmp.cmp(expected_output_path, output_path, shallow=False)
+    assert same_content
+
+
+def test_convert_line():
+    assert (
+        convert_line("in the end it doesn't even matter", 27.89)
+        == "in the end it doesn't even matter #00:00:27-8#"
     )
+
+
+def test_convert_line_beginning_with_space():
+    assert (
+        convert_line(" in the end it doesn't even matter", 27.89)
+        == "in the end it doesn't even matter #00:00:27-8#"
+    )
+
+
+def test_convert_timestamp():
+    assert convert_timestamp(27.89) == "#00:00:27-8#"
+
+
+def test_convert_timestamp_with_hours_and_minutes():
+    assert convert_timestamp(99089.345) == "#27:31:29-3#"
